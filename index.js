06cc6415b5b3d204157917c8e825a730a0a33947
Asif Mahmud <asifmahmud3472@gmail.com>
2025-01-28 20:03:59 -0500

    fixed private key issue

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ const fileUpload = require('express-fileupload')
 const app = express();
 const port = 4000;
 const admin = require('firebase-admin');
-const { privateKey } = JSON.parse(process.env.PRIVATE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert({
       "type": process.env.JWT_TYPE,
       "project_id": process.env.PROJECT_ID,
       "private_key_id": process.env.PRIVATE_KEY_ID,
-      "private_key": privateKey,
+      "private_key": process.env.PRIVATE_KEY,
       "client_email": process.env.CLIENT_EMAIL,
       "client_id": process.env.CLIENT_ID,
       "auth_uri": process.env.AUTH_URI,
